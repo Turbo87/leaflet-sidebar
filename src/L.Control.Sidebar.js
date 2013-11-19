e24f3538f35a1c79472b9cd112b5e0305858ee9f
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2013-11-19 17:56:40 +0100

    Fire 'show' and 'hide' event on sidebar objects

--- src/L.Control.Sidebar.js
+++ src/L.Control.Sidebar.js
@@ -1,4 +1,7 @@
 L.Control.Sidebar = L.Control.extend({
+
+    includes: L.Mixin.Events,
+
     options: {
         closeButton: true,
         position: 'left'
@@ -75,6 +78,7 @@ L.Control.Sidebar = L.Control.extend({
             this._map.panBy([-this.getOffset() / 2, 0], {
                 duration: 0.5
             });
+            this.fire('show');
         }
     },
 
@@ -84,6 +88,7 @@ L.Control.Sidebar = L.Control.extend({
             this._map.panBy([this.getOffset() / 2, 0], {
                 duration: 0.5
             });
+            this.fire('hide');
         }
     },
 
